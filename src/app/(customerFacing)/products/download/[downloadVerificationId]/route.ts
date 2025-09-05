62854fdea0120f90f7b8e77a35f7a55c750b2794
Aliona <ashelyaa17@gmail.com>
2025-09-05 16:56:50 +0300

    fix Type error

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import fs from "fs/promises";
 import db from "@/src/db/db";
 
-export async function GET(
-  req: NextRequest,
-  {
-    params: { downloadVerificationId },
-  }: { params: { downloadVerificationId: string } }
-) {
+export async function GET(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const segments = pathname.split("/");
+  const downloadVerificationId = segments[segments.length - 1];
+
   const data = await db.downloadVerification.findUnique({
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
     select: { product: { select: { filePath: true, name: true } } },
@@ -18,15 +17,13 @@ export async function GET(
   }
 
   const { size } = await fs.stat(data.product.filePath);
-  const fileBuffer = await fs.readFile(data.product.filePath);
+  const file = await fs.readFile(data.product.filePath);
   const extension = data.product.filePath.split(".").pop();
 
-  return new NextResponse(fileBuffer, {
+  return new NextResponse(file, {
     headers: {
       "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
       "Content-Length": size.toString(),
     },
   });
 }
-
-export {};
